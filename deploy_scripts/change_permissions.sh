2b1e9c62ae8a69baddd65f27bad45603fdab313f
Abdun Nur Tomal <tomal.nur@gmail.com>
2020-10-06 12:55:13 +0600

    local

--- deploy_scripts/change_permissions.sh
+++ deploy_scripts/change_permissions.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Fix user rights
-sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
-
-#echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+sudo usermod -a -G node ec2-user
+sudo chown -R ec2-user:node calculator
+sudo chmod 2775 calculator
+find calculator -type d -exec sudo chmod 2775 {} \;
+find calculator -type f -exec sudo chmod 0664 {} \;
