738db7318346bfb4a4f4db43db02e97009346bdd
Abdun Nur Tomal <tomal.nur@gmail.com>
2020-10-06 13:12:59 +0600

    local

--- deploy_scripts/change_permissions.sh
+++ deploy_scripts/change_permissions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Fix user rights
-sudo usermod -a -G node ec2-user
-sudo chown -R ec2-user:node calculator
+sudo usermod -a -G ec2-user ec2-user
+sudo chown -R ec2-user:ec2-user calculator
 sudo chmod 2775 calculator
 find calculator -type d -exec sudo chmod 2775 {} \;
 find calculator -type f -exec sudo chmod 0664 {} \;
